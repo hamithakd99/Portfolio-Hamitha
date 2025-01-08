a118b9692be5b532cad1d66f2bf3a6e91c2b36cd
Hamitha_KD <hamithakd99@gmail.com>
2025-01-08 12:51:10 +0530

    complete header section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 'use client'
 
+import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
 
 export default function Home() {
   return (
     <>
     <Navbar />
+    <Header />
     </>
   );
 }
