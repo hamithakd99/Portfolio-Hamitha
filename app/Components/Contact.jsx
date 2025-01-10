d0688dd599015a72b7fc98743445be225a4c28ee
Hamitha_KD <hamithakd99@gmail.com>
2025-01-10 11:27:45 +0530

    make responsiveness of the contact section

--- app/Components/Contact.jsx
+++ app/Components/Contact.jsx
@@ -1,3 +1,5 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const Contact = () => {
@@ -9,12 +11,14 @@ const Contact = () => {
         I'd love to hear from you! If you have any questions, comments or feedback, please use the form below
       </p>
       <form action="" className='max-w-2xl mx-auto'>
-        <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
-          <input type="text" placeholder='Enter Your name' required className=''/>
-          <input type="email" placeholder='Enter Your email' required/>
+        <div className='flex flex-col items-center md:flex-row gap-6 mt-10 mb-8 justify-between'>
+          <input type="text" placeholder='Enter Your name' required className='p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white w-full'/>
+          <input type="email" placeholder='Enter Your email' required className='p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white w-full'/>
         </div>
-        <textarea rows='6' name="" id="" placeholder='Enter Your message' required className='resize-none'></textarea>
-        <button type="submit">Submit Now</button>
+        <textarea rows='6' name="" id="" placeholder='Enter Your message' required className='resize-none w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6'></textarea>
+        <button type="submit" className='border border-gray-400 px-6 py-4 rounded-full w-max bg-black/80 text-white flex items-center mx-auto hover:bg-black duration-500'>Submit Now <Image src={assets.right_arrow_white} alt='' className='w-4 ml-5'/></button>
+
+        <p className='mt-4'>Sending...</p>
       </form>
     </div>
   )
